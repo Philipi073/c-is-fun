c14125b876883ab625843868e241211548c23b52
Okoye Philip <okoyephlp07322@gmail.com>
2023-01-14 05:20:02 -0800

    fibo

---
+++ 0x00-recursion/3-fibo.c
@@ -0,0 +1,16 @@
+/* fibonachi series using recursion*/
+#include <stdio.h>
+int fib(int n)
+{
+	if (n < 1)
+		return n;
+	return fib(n - 1) + fib(n - 2);
+}
+
+int main(void)
+{
+	int n = 9;
+	printf("%d", fib(n));
+	getchar();
+	return 0;
+}
